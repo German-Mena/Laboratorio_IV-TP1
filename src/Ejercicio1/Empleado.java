c2eeb0a0e3c52dfbf5d0b463e9204f831b314144
Lucas <lucgom99@gmail.com>
2022-08-09 17:35:42 -0300

    Getters and Setters

--- src/Ejercicio1/Empleado.java
+++ src/Ejercicio1/Empleado.java
@@ -4,4 +4,26 @@ public class Empleado {
 	private int id;
 	private String nombre;
 	private int edad;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
 }
